6fd82cab246f90a05342335c1fc1fb106ffc5184
brunneis <dev@brunneis.com>
2024-06-17 16:52:26 +0200

    Set API_PROCESSES_COUNT default to 1

--- server/scripts/start-server.sh
+++ server/scripts/start-server.sh
@@ -2,7 +2,7 @@
 cd $(dirname $0)/..
 
 if [ -z "$API_PROCESSES_COUNT" ]; then
-    API_PROCESSES_COUNT=8
+    API_PROCESSES_COUNT=1
 fi
 
 python -m socketify main:app --host 0.0.0.0 --port 53256 --workers $API_PROCESSES_COUNT
